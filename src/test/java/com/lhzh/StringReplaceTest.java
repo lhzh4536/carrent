cbbd11063b1e1a0872ce08eeb61e1ceeaa9a9df5
lhzh4536 <lhzh4536@163.com>
2020-03-29 23:24:26 +0800

    改为maven项目

---
+++ src/test/java/com/lhzh/StringReplaceTest.java
@@ -0,0 +1,15 @@
+package com.lhzh;
+
+import org.junit.Test;
+
+public class StringReplaceTest {
+
+    @Test
+    public void Test1_Replcace(){
+        String carimg="aaa.txt";
+        String suffix ="aaa";
+        String replace = carimg.replace(suffix, "bbb");
+        System.out.println(replace);
+    }
+
+}
